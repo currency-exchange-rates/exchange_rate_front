2f2a254a690463e67abab2788d196b56bfc022a3
Alina Abrashkina <lina.lina.malinaa@gmail.com>
2024-12-17 17:52:01 +0300

    added: adaptive, fix layout

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,5 @@
 import './styles/index.css';
 import { CurrencyRates, Elements } from './types';
-
-import { subtract } from "./utils/app";
 import { handlerCurrency } from './utils/currency-handler';
 
 // моковые данные для теста функции
@@ -36,22 +34,3 @@ elements.form.addEventListener('input', () => {
     console.error(error);
   }
 })
-
-// Тестовая функция
-function init() {
-  const form = document.querySelector("form");
-  form?.addEventListener("submit", submitHandler);
-}
-
-function submitHandler(e: Event) {
-  e.preventDefault();
-  const num1 = document.querySelector("input[name='firstnumber']") as HTMLInputElement;
-  const num2 = document.querySelector("input[name='secondnumber']") as HTMLInputElement;
-  const result = subtract(Number(num1.value), Number(num2.value));
-  const resultElement = document.querySelector("p");
-  if (resultElement) {
-    resultElement.textContent = result.toString();
-  }
-}
-
-init();
--- src/utils/app.ts
+++
@@ -1,5 +0,0 @@
-// Тестовая функция, экспортируется в index.ts
-
-export function subtract(firstnumber: number, secondnumber: number): number {
-  return firstnumber - secondnumber;
-}
